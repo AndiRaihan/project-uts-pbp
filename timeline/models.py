c1a0af7b708fe099c71886318952e147cdce7afa
AndiAyuna14 <andiayuna14@gmail.com>
2022-10-29 17:07:32 -0700

    push views timeline

--- timeline/models.py
+++ timeline/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from auth.models import UserProfile
+from authen.models import UserProfile, Content
 from forum.models import Forum
 
 # Create your models here.
 class Comment(models.Model):
-    user = models.ForeignKey(UserProfile,on_delete=models.CASCADE)
-    reply = models.OneToOneField(Forum)
-    date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    date_created = models.DateField(auto_now_add=True)
+    comment = models.TextField()
+    commented_on = models.ForeignKey(Content, on_delete=models.CASCADE)
