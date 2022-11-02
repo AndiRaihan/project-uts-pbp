db1c4d27196adb268c72a7eb2c483a787ebc1f10
AndiRaihan <dzaky.raihan@gmail.com>
2022-11-02 19:27:50 +0700

    Added ajax

--- timeline/urls.py
+++ timeline/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 
     path('group/<str:group_name>/', show_timeline, name='timeline'),
     path('group/<str:group_name>/<int:content_id>/comment/', comment, name='comment'),
+    path('group/<str:group_name>/<int:content_id>/comment/json/', comment_json, name='comment'),
+    path('group/<str:group_name>/<int:content_id>/comment/add/', add_comment, name='comment'),
     path('', show_group, name='landing'),
     path('group/<str:group_name>/<int:content_id>/delete/', delete, name='delete'),
     path('group/<str:group_name>/<int:content_id>/upvote/', upvote, name='upvote'),
