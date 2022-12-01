2e678e7b5b7e81cae5b385821f924175e87a58bc
munifah.nurfadhilah <munifah.nurfadhilah@ui.ac.id>
2022-12-01 10:52:57 +0700

    progress

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from authentication.views import login
+from authentication.views import login, register, logout
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='json'),
+    path('register/', register, name='json'),
+    path('logout/', logout, name='json'),
 ]
